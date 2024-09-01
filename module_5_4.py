5f9ce3a436b2614a17770fa79343973873e2e6db
GlaVl <Darkwatcher2008@mail.ru>
2024-09-01 18:27:00 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,32 @@
+class House:
+    houses_history = []
+    
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __init__(self, name, numbers_of_floor):
+        self.name = name
+        self.numbers_of_floor = numbers_of_floor
+
+    def go_to(self, new_floor):
+        if new_floor > int(self.numbers_of_floor) or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+    def __del__(self):
+        print(f"{self.name} снесен, но он останется в истории")
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
